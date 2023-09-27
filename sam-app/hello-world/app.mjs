8420acb2ce9815c5683d0a072a838207bde05104
Mukul Phougat <mukulphoughat@gmail.com>
2023-09-27 06:49:58 +0000

    .

--- sam-app/hello-world/app.mjs
+++ sam-app/hello-world/app.mjs
@@ -1,15 +1,15 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+// /**
+//  *
+//  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+//  * @param {Object} event - API Gateway Lambda Proxy Input Format
+//  *
+//  * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+//  * @param {Object} context
+//  *
+//  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+//  * @returns {Object} object - API Gateway Lambda Proxy Output Format
+//  * 
+//  */
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
@@ -49,11 +49,7 @@ export const lambdaHandler = async (event, context) => {
             })
           );
         return {
-          headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "http://localhost:3000", // Allow from anywhere 
-            "Access-Control-Allow-Methods": "GET" // Allow only GET request 
-          },
+          
           statusCode: 200,
           body: JSON.stringify(user.Items),
         };
